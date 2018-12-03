1b197f78a18d80b2ee40f504fc641980cc291f6b
Anthony Lloyd <anthony.lloyd@alphakinetic.com>
2018-12-03 08:41:34 +0000

    try default in 0

--- Fsion/MapSlim.fs
+++ Fsion/MapSlim.fs
@@ -11,15 +11,15 @@ type private Entry<'TKey,'TValue> =
     val mutable next : int
 
 type private InitialHolder<'TKey,'TValue>() =
-    static let initial = Array.zeroCreate<Entry<'TKey,'TValue>> 1
+    static let initial = Array.zeroCreate<Entry<'TKey,'TValue>> 2
     static member inline Initial = initial
 
 type MapSlim<'TKey,'TValue when 'TKey : equality and 'TKey :> IEquatable<'TKey>> =
-    val mutable private count : int
+    val mutable private countWithDefault : int
     val mutable private entries : Entry<'TKey,'TValue>[]
-    new() = {count=0; entries=InitialHolder.Initial}
+    new() = {countWithDefault=1; entries=InitialHolder.Initial}
     new(capacity:int) = {
-        count = 0
+        countWithDefault = 1
         entries =
             let inline powerOf2 v =
                 if v &&& (v-1) = 0 then v
@@ -31,40 +31,39 @@ type MapSlim<'TKey,'TValue when 'TKey : equality and 'TKey :> IEquatable<'TKey>>
             powerOf2 capacity |> Array.zeroCreate
     }
 
-    member m.Count = m.count
+    member m.Count = m.countWithDefault-1
 
     member private m.Resize() =
         let oldEntries = m.entries
-        let entries = Array.zeroCreate<Entry<_,_>> (oldEntries.Length*2)
-        for i = oldEntries.Length-1 downto 0 do
-            entries.[i].value <- oldEntries.[i].value
-            entries.[i].key <- oldEntries.[i].key
-            let bi = entries.[i].key.GetHashCode() &&& (entries.Length-1)
-            entries.[i].next <- entries.[bi].bucket-1
-            entries.[bi].bucket <- i+1
+        let entries = Array.zeroCreate<Entry<_,_>> (oldEntries.Length*2-1)
+        if oldEntries.Length <> 2 then
+            for i = oldEntries.Length-1 downto 1 do
+                entries.[i].value <- oldEntries.[i].value
+                entries.[i].key <- oldEntries.[i].key
+                let bi = (entries.[i].key.GetHashCode() &&& (entries.Length-2)) + 1
+                entries.[i].next <- entries.[bi].bucket
+                entries.[bi].bucket <- i
         m.entries <- entries
 
     [<MethodImpl(MethodImplOptions.NoInlining)>]
     member private m.AddKey(key:'TKey, hashCode:int) =
-        let i = m.count
-        if i = 0 && m.entries.Length = 1 then
-            m.entries <- Array.zeroCreate 2
-        elif i = m.entries.Length then m.Resize()
+        let i = m.countWithDefault
+        if i = m.entries.Length || (i = 1 && m.entries.Length = 2) then m.Resize()
         let entries = m.entries
         entries.[i].key <- key
-        let bucketIndex = hashCode &&& (entries.Length-1)
-        entries.[i].next <- entries.[bucketIndex].bucket-1
-        entries.[bucketIndex].bucket <- i+1
-        m.count <- i+1
+        let bucketIndex = (hashCode &&& (entries.Length-2)) + 1
+        entries.[i].next <- entries.[bucketIndex].bucket
+        entries.[bucketIndex].bucket <- i
+        m.countWithDefault <- i+1
         &entries.[i].value
 
     member m.Set(key:'TKey, value: 'TValue) =
         let entries = m.entries
         let hashCode = key.GetHashCode()
-        let mutable i = entries.[hashCode &&& (entries.Length-1)].bucket-1
-        while i <> -1 && not(key.Equals(entries.[i].key)) do
+        let mutable i = entries.[(hashCode &&& (entries.Length-2)) + 1].bucket
+        while i <> 0 && not(key.Equals(entries.[i].key)) do
             i <- entries.[i].next
-        if i = -1 then
+        if i = 0 then
             let v = &m.AddKey(key, hashCode)
             v <- value
         else entries.[i].value <- value
@@ -72,19 +71,19 @@ type MapSlim<'TKey,'TValue when 'TKey : equality and 'TKey :> IEquatable<'TKey>>
     member m.GetRef(key:'TKey) : 'TValue byref =
         let entries = m.entries
         let hashCode = key.GetHashCode()
-        let mutable i = entries.[hashCode &&& (entries.Length-1)].bucket-1
-        while i <> -1 && not(key.Equals(entries.[i].key)) do
+        let mutable i = entries.[(hashCode &&& (entries.Length-2)) + 1].bucket
+        while i <> 0 && not(key.Equals(entries.[i].key)) do
             i <- entries.[i].next
-        if i = -1 then &m.AddKey(key, hashCode)
+        if i = 0 then &m.AddKey(key, hashCode)
         else &entries.[i].value
 
     member m.GetRef(key:'TKey, added: bool outref) : 'TValue byref =
         let entries = m.entries
         let hashCode = key.GetHashCode()
-        let mutable i = entries.[hashCode &&& (entries.Length-1)].bucket-1
-        while i <> -1 && not(key.Equals(entries.[i].key)) do
+        let mutable i = entries.[(hashCode &&& (entries.Length-2)) + 1].bucket
+        while i <> 0 && not(key.Equals(entries.[i].key)) do
             i <- entries.[i].next
-        if i = -1 then
+        if i = 0 then
             added <- true
             &m.AddKey(key, hashCode)
         else
@@ -93,14 +92,14 @@ type MapSlim<'TKey,'TValue when 'TKey : equality and 'TKey :> IEquatable<'TKey>>
 
     member m.GetOption(key:'TKey) : 'TValue voption =
         let entries = m.entries
-        let mutable i = entries.[key.GetHashCode() &&& (entries.Length-1)].bucket-1
-        while i <> -1 && not(key.Equals(entries.[i].key)) do
+        let mutable i = entries.[(key.GetHashCode() &&& (entries.Length-2)) + 1].bucket
+        while i <> 0 && not(key.Equals(entries.[i].key)) do
             i <- entries.[i].next
-        if i = -1 then ValueNone
+        if i = 0 then ValueNone
         else ValueSome entries.[i].value
 
     member m.Item(i) : 'TKey * 'TValue =
-        let entries = m.entries.[i]
+        let entries = m.entries.[i+1]
         entries.key, entries.value
 
 [<AutoOpen>]
